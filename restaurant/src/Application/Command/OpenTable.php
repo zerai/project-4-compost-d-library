cbe37b7eb88c1901f62b83f3d36dbc495eecde47
zero <teclaizerai@gmail.com>
2020-10-21 13:12:16 +0200

    Add OpenTable command.

---
+++ restaurant/src/Application/Command/OpenTable.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Restaurant\Application\Command;
+
+use Restaurant\Domain\Model\Table\TableId;
+
+class OpenTable
+{
+    public TableId $tableId;
+    public int $tableNumber;
+    public string $waiter;
+
+    public function __construct(string $tableId, int $tableNumber, string $waiter)
+    {
+        $this->tableId = TableId::fromString($tableId);
+        $this->tableNumber = $tableNumber;
+        $this->waiter = $waiter;
+    }
+
+    public function tableId(): TableId
+    {
+        return $this->tableId;
+    }
+
+    public function tableNumber(): int
+    {
+        return $this->tableNumber;
+    }
+
+    public function waiter(): string
+    {
+        return $this->waiter;
+    }
+
+
+}
