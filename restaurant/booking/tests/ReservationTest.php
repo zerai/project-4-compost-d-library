c28af67ea495208e01b0cdc5f616c2c8f2999b1a
zero <teclaizerai@gmail.com>
2020-12-11 18:30:21 +0100

    Setup booking bounded context.

---
+++ restaurant/booking/tests/ReservationTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Restaurant\Booking\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ReservationTest extends TestCase
+{
+    /** @test */
+    public function foo(): void
+    {
+        self::assertTrue(true);
+    }
+}
